2e89a292a428149aebb2eb2ed2b8dfe7c2f7ca46
gabnunes0204 <gabriel.microsmp@gmail.com>
2023-08-15 17:18:16 -0300

    Criação do ModelMapperConfig e utilização do mesmo

--- src/main/java/br/com/estudo/demo/resource/UserResource.java
+++ src/main/java/br/com/estudo/demo/resource/UserResource.java
@@ -1,24 +1,27 @@
 package br.com.estudo.demo.resource;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import br.com.estudo.demo.domain.User;
+import br.com.estudo.demo.domain.dto.UserDTO;
 import br.com.estudo.demo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
-
+	
+	@Autowired
+	private  ModelMapper mapper;
+	
 	@Autowired
 	private UserService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Integer id) {
-		return ResponseEntity.ok().body(service.findById(id));
+	public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+		return ResponseEntity.ok().body(mapper.map(service.findById(id), UserDTO.class));
 	}
 }
