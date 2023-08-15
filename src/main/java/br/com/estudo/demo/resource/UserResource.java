6db072c9fcd43ac4a71722a7b44ba1ae9be06321
gabnunes0204 <gabriel.microsmp@gmail.com>
2023-08-15 13:58:25 -0300

    Adiciona perfis de testes

--- src/main/java/br/com/estudo/demo/resource/UserResource.java
+++ src/main/java/br/com/estudo/demo/resource/UserResource.java
@@ -1,5 +1,6 @@
 package br.com.estudo.demo.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,14 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.estudo.demo.domain.User;
+import br.com.estudo.demo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<User> findById(@PathVariable Integer id) {
 		
-		return ResponseEntity.ok().body(new User(1, "Gabriel Nunes", "gabriel@gmail.com", "12345"));
+		return ResponseEntity.ok().body(service.findById(id));
 	}
 }
---
+++ src/main/java/br/com/estudo/demo/services/UserService.java
@@ -0,0 +1,8 @@
+package br.com.estudo.demo.services;
+
+import br.com.estudo.demo.domain.User;
+
+public interface UserService {
+
+	User findById(Integer id);
+}
